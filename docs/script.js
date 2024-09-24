93290bf165eb3fcb69b86f59b7525ab715b6ccda
Prakit <saratgogoi78@gmail.com>
2024-09-24 21:49:57 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,10 @@
-// Add event listener to button
+// Add event listeners to buttons
 document.querySelector('.get-knowledge-button').addEventListener('click', function() {
     // Redirect to knowledge page
     window.location.href = 'knowledge.html';
+});
+
+document.querySelector('.explore-button').addEventListener('click', function() {
+    // Redirect to stories page
+    window.location.href = 'stories.html';
 });
