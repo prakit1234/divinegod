4bee0790d1af17dac5b0862132b7496ef87a80f0
Prakit <saratgogoi78@gmail.com>
2024-09-24 21:45:07 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,5 @@
-
+// Add event listener to button
+document.querySelector('.get-knowledge-button').addEventListener('click', function() {
+    // Redirect to knowledge page
+    window.location.href = 'knowledge.html';
+});
