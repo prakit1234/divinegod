5115a33f8f2509df6c77a27aaf8a061cbe418a52
Prakit <saratgogoi78@gmail.com>
2024-09-26 17:35:04 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,10 @@
 // Add event listeners to buttons
 document.querySelector('.get-knowledge-button').addEventListener('click', function() {
     // Redirect to knowledge page
-    window.location.href = 'knowledge.html';
+    window.location.href = 'https://loginregister-ruby.vercel.app/';
 });
 
 document.querySelector('.explore-button').addEventListener('click', function() {
     // Redirect to stories page
-    window.location.href = 'stories.html';
+    window.location.href = 'https://loginregister-ruby.vercel.app/';
 });
