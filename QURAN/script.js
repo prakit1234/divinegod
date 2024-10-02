993a6cacdc074ee191c6f2b4f2c57054bd7b86ff
Prakit <saratgogoi78@gmail.com>
2024-10-02 18:05:49 +0530

    Create script.js

---
+++ QURAN/script.js
@@ -0,0 +1,7 @@
+function downloadPDF() {
+  window.location.href = 'https://www.mediafire.com/file/vyevneq6dltxyic/quran.pdf/file'; // Replace with actual download link for the PDF
+}
+
+function downloadMP3() {
+  window.location.href = 'https://www.allahsword.com/quran_with_english.html'; // Replace with actual download link for the MP3
+}
