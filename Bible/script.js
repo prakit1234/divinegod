4c2142147164e0d50ef94e683745126f8f0597c1
Prakit <saratgogoi78@gmail.com>
2024-10-03 16:06:23 +0530

    Create script.js

---
+++ Bible/script.js
@@ -0,0 +1,7 @@
+function downloadPDF() {
+  window.location.href = 'r'; // Replace with actual download link for the PDF
+}
+
+function downloadMP3() {
+  window.location.href = 'r'; // Replace with actual download link for the MP3
+}
